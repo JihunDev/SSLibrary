ff939fbf0a219c07003ef1ea5ba9d6fefa9b4536
sangyi0110 <aofurtkddl@gmail.com>
2015-06-29 09:01:25 +0000

    userbookbiz, dao 업데이트

--- SSLibrary/src/com/book/UserBookBiz.java
+++ SSLibrary/src/com/book/UserBookBiz.java
@@ -10,12 +10,16 @@ import com.frame.Biz;
 import com.frame.Dao;
 import com.frame.SearchBiz;
 import com.frame.SearchDao;
+import com.frame.UpdateAndReturnBiz;
+import com.frame.UpdateAndReturnDao;
 @Service("userbookbiz")
-public class UserBookBiz implements Biz, SearchBiz {
+public class UserBookBiz implements Biz, SearchBiz, UpdateAndReturnBiz {
 	@Resource(name="userbookdao")
 	Dao dao;
 	@Resource(name="userbookdao")
 	SearchDao dao2;
+	@Resource(name="userbookdao")
+	UpdateAndReturnDao dao3;
 	
 	@Override
 	public Object register(Object obj) throws Exception {
@@ -69,6 +73,17 @@ public class UserBookBiz implements Biz, SearchBiz {
 	@Override
 	public ArrayList<Object> getid(Object obj) throws Exception {
 		return dao2.searchid(obj);
+	}
+
+	@Override
+	public Object logupdate(Object obj) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object logreturn(Object obj) throws Exception {
+		return dao3.logreturn(obj);
 	}
 
 }

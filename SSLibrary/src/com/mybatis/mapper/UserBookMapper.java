fff799ea8fa18917b22ee543e7b219f62060ae0f
sangyi0110 <sangyi0110@19c9ebcf-3a19-0510-891f-ab9eafe7a1e6>
2015-07-01 04:07:48 +0000

    userbook xml  sql 추가 및 userbookmapper 추가

--- SSLibrary/src/com/mybatis/mapper/UserBookMapper.java
+++ SSLibrary/src/com/mybatis/mapper/UserBookMapper.java
@@ -10,7 +10,8 @@ public interface UserBookMapper {
 	public int updateuserbook(Object obj);
 	public ArrayList<Object> selectiduserbooks(Object obj);
 	public int updatereturnuserbook(Object obj);
-	public ArrayList<Object> selectreturnuserbooks(Object obj); //반납이 y인 유저들의 정보 
+	public ArrayList<Object> selectreturnuserbooks(Object obj); //반납이 y인 유저들의 정보
+	public ArrayList<Object> selectoverduebooks(); //연체된 사람들 정지회원으로 만들기
 	
 	
 }
